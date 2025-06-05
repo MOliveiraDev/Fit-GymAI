f021a61a692a886492a66812b2b988c4b1ddddfc
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-06-04 21:04:15 -0300

    first commit

---
+++ src/main/java/gemini/FitGymGpt/FitGymGptApplication.java
@@ -0,0 +1,13 @@
+package gemini.FitGymGpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitGymGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitGymGptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gemini/FitGymGpt/FitGymGptApplicationTests.java
@@ -0,0 +1,13 @@
+package gemini.FitGymGpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitGymGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
