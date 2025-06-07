579cb4a39d53cd82af95f75e3428a687c4543b68
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-06-06 22:34:48 -0300

    feat: add UserServicesImpl

--- src/main/java/gemini/FitGymGpt/Service/User/UserServiceImpl.java
+++ src/main/java/gemini/FitGymGpt/Service/User/UserServiceImpl.java
@@ -1,4 +1,20 @@
 package gemini.FitGymGpt.Service.User;
 
-public class UserServiceImpl {
+import gemini.FitGymGpt.DataBase.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserDetailsService {
+
+        private final UserRepository userRepository;
+
+        @Override
+    public UserDetails loadUserByUsername(String username) {
+            return userRepository.findByUsername(username)
+                    .orElseThrow(() -> new RuntimeException("User not found with username: " + username));
+        }
 }
