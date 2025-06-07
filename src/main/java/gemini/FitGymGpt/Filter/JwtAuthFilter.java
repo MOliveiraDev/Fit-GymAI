f7274ba3f8bd12e78575c8f6d44e9ea4968b6f98
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-06-06 22:34:27 -0300

    feat: add Jwt Filter

---
+++ src/main/java/gemini/FitGymGpt/Filter/JwtAuthFilter.java
@@ -0,0 +1,58 @@
+package gemini.FitGymGpt.Filter;
+
+import gemini.FitGymGpt.DataBase.Model.User;
+import gemini.FitGymGpt.Service.Jwt.JwtService;
+import gemini.FitGymGpt.Service.User.UserServiceImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    private final UserServiceImpl userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String username;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+        username = jwtService.extractUsername(jwt);
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            User userDetails = (User) userService.loadUserByUsername(username);
+
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/gemini/FitGymGpt/Service/User/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package gemini.FitGymGpt.Service.User;
+
+public class UserServiceImpl {
+}
