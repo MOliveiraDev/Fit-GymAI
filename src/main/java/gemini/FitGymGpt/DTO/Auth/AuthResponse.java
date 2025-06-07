f1347f65d8a3f996c5477350b61a6956d6b5f701
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-06-07 16:24:19 -0300

    Fix: add Annotation Data, AllArgsContructor

--- src/main/java/gemini/FitGymGpt/DTO/Auth/AuthResponse.java
+++ src/main/java/gemini/FitGymGpt/DTO/Auth/AuthResponse.java
@@ -1,5 +1,11 @@
 package gemini.FitGymGpt.DTO.Auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
 public class AuthResponse {
 
     private String token;
