6bf15bf8b769d6baf437cc408ea4544a170ad240
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-06-08 09:58:05 -0300

    Feat: add Gemini Response/Request

---
+++ src/main/java/gemini/FitGymGpt/DTO/Gemini/GeminiResponse.java
@@ -0,0 +1,31 @@
+package gemini.FitGymGpt.DTO.Gemini;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GeminiResponse {
+
+    private List<Candidate> candidates;
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Candidate {
+        private List<Content> contents;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Content {
+        private List<Part> parts;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Part {
+        private String text;
+    }
+}
