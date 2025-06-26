0ed08de8007ec0be931b2a0ac9c68a40eebdb738
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-06-26 11:19:29 -0300

    Feat: add IWorkPlanController interface for work plan generation

---
+++ src/main/java/gemini/FitGymGpt/Controller/Gemini/IWorkPlanController.java
@@ -0,0 +1,20 @@
+package gemini.FitGymGpt.Controller.Gemini;
+
+
+import gemini.FitGymGpt.DTO.Gemini.BodyStatsRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface IWorkPlanController {
+
+    @Operation(summary = "Gera um plano de treino para o usuário")
+    @ApiResponses ({
+         @ApiResponse(responseCode = "200", description = "Plano de treino gerado com sucesso"),
+         @ApiResponse(responseCode = "400", description = "Erro ao gerar plano de treino")
+    })
+    ResponseEntity<?> createWorkPlan(@PathVariable Long userId, @RequestBody BodyStatsRequest request);
+}
