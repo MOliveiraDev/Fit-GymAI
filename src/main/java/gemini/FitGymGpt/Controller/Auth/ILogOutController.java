678f00d3fcfe9ebd0a0237fdd83edb29d77a8a9f
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-06-26 11:19:18 -0300

    Feat: add admin registration endpoint to allow admin user creation

--- src/main/java/gemini/FitGymGpt/Controller/Auth/ILogOutController.java
+++ src/main/java/gemini/FitGymGpt/Controller/Auth/ILogOutController.java
@@ -1,4 +1,18 @@
 package gemini.FitGymGpt.Controller.Auth;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
+
 public interface ILogOutController {
+
+    @Operation(summary = "Logout de usuários")
+    @ApiResponses ({
+        @ApiResponse(responseCode = "200", description = "Logout bem-sucedido"),
+        @ApiResponse(responseCode = "400", description = "Erro ao realizar logout")
+    })
+    ResponseEntity<String> logout(@RequestHeader(value = "Authorization", required = false) String authorizationHeader);
 }
