77dbf5801aa6fe2d15e573e40e9048651532059e
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-06-26 20:29:37 -0300

    Refactor: remove inheritance from AdminInit in FitGymGptApplication

--- src/main/java/gemini/FitGymGpt/FitGymGptApplication.java
+++ src/main/java/gemini/FitGymGpt/FitGymGptApplication.java
@@ -1,18 +1,11 @@
 package gemini.FitGymGpt;
 
-import gemini.FitGymGpt.Config.AdminInit;
-import gemini.FitGymGpt.DataBase.Repository.UserRepository;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class FitGymGptApplication extends AdminInit {
-
-	public FitGymGptApplication(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-		super(userRepository, passwordEncoder);
-	}
+public class FitGymGptApplication {
 
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
