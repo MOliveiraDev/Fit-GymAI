8cd4c56c98041333742702e474c585d831a2e245
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-06-26 11:18:27 -0300

    Feat: add AdminInit component to initialize admin user on application startup

---
+++ src/main/java/gemini/FitGymGpt/Config/AdminInit.java
@@ -0,0 +1,29 @@
+package gemini.FitGymGpt.Config;
+
+import gemini.FitGymGpt.DataBase.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AdminInit implements ApplicationRunner {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        if (!userRepository.existsByUsername("admin")) {
+            gemini.FitGymGpt.DataBase.Model.User admin = gemini.FitGymGpt.DataBase.Model.User.builder()
+                    .username("admin")
+                    .password(passwordEncoder.encode("admin123"))
+                    .role(gemini.FitGymGpt.DataBase.Model.Role.ADMIN)
+                    .build();
+            userRepository.save(admin);
+            System.out.println("ADM account: username: admin password: admin123");
+        }
+    }
+}
