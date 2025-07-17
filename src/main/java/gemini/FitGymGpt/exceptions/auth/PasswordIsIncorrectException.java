965bd44b5a83b68c9510e73cf381a18848427683
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-07-17 12:39:12 -0300

    Feat: remove overloaded constructor from PasswordIsIncorrectException

--- src/main/java/gemini/FitGymGpt/exceptions/auth/PasswordIsIncorrectException.java
+++ src/main/java/gemini/FitGymGpt/exceptions/auth/PasswordIsIncorrectException.java
@@ -5,8 +5,4 @@ public class PasswordIsIncorrectException extends RuntimeException {
     public PasswordIsIncorrectException(String username) {
         super("Senha incorreta para o usuário " + username);
     }
-
-    public PasswordIsIncorrectException(String username, Throwable cause) {
-        super("Senha incorreta para o usuário " + username, cause);
-    }
 }
