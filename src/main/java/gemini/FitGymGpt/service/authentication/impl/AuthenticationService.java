56d640d470050e4ee00812c45bad47af1c747a01
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-07-17 12:34:52 -0300

    Feat: implement AuthenticationService for user login functionality

---
+++ src/main/java/gemini/FitGymGpt/service/authentication/impl/AuthenticationService.java
@@ -0,0 +1,40 @@
+package gemini.FitGymGpt.service.authentication.impl;
+
+import gemini.FitGymGpt.dto.auth.AuthRequest;
+import gemini.FitGymGpt.dto.auth.AuthResponse;
+import gemini.FitGymGpt.exceptions.auth.EmailNotFoundException;
+import gemini.FitGymGpt.service.authentication.IAuthenticationService;
+import gemini.FitGymGpt.service.jwt.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService implements IAuthenticationService {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    @Override
+    public AuthResponse login(AuthRequest request) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+            );
+
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            String jwtToken = jwtService.generateToken(userDetails);
+
+            return AuthResponse.builder()
+                    .token(jwtToken)
+                    .build();
+        } catch (BadCredentialsException e) {
+            throw new EmailNotFoundException("Email ou senha inválidos");
+        }
+    }
+}
