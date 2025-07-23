027685bc52ffd4dc931c89fdfde2f6772d7cca56
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-07-23 11:05:48 -0300

    removed

--- src/main/java/gemini/FitGymGpt/service/authentication/IAuthenticationService.java
+++
@@ -1,9 +0,0 @@
-package gemini.FitGymGpt.service.authentication;
-
-import gemini.FitGymGpt.dto.auth.AuthRequest;
-import gemini.FitGymGpt.dto.auth.AuthResponse;
-
-public interface IAuthenticationService {
-
-    AuthResponse login(AuthRequest request) throws Exception;
-}
--- src/main/java/gemini/FitGymGpt/service/register/IRegisterService.java
+++
@@ -1,9 +0,0 @@
-package gemini.FitGymGpt.service.register;
-
-import gemini.FitGymGpt.dto.register.RegisterRequest;
-import gemini.FitGymGpt.dto.register.RegisterResponse;
-
-public interface IRegisterService {
-
-    RegisterResponse register(RegisterRequest registerRequest) throws Exception;
-}
