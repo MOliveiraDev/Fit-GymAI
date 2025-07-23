a1a7200aefb85a25139a8b734c0c30e695ffff9f
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-07-23 11:02:35 -0300

    Feat: add registerAdmin method

--- src/main/java/gemini/FitGymGpt/controller/auth/Impl/AuthControllerImpl.java
+++ src/main/java/gemini/FitGymGpt/controller/auth/Impl/AuthControllerImpl.java
@@ -1,19 +1,15 @@
 package gemini.FitGymGpt.controller.auth.Impl;
 
 import gemini.FitGymGpt.controller.auth.IAuthController;
-import gemini.FitGymGpt.database.repository.UserRepository;
 import gemini.FitGymGpt.dto.auth.AuthRequest;
 import gemini.FitGymGpt.dto.auth.AuthResponse;
 import gemini.FitGymGpt.dto.register.RegisterRequest;
 import gemini.FitGymGpt.dto.register.RegisterResponse;
-import gemini.FitGymGpt.service.authentication.impl.AuthenticationService;
-import gemini.FitGymGpt.service.jwt.JwtService;
-import gemini.FitGymGpt.service.register.impl.RegisterService;
+import gemini.FitGymGpt.service.auth.AuthenticationService;
+import gemini.FitGymGpt.service.auth.RegisterService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -31,8 +27,15 @@ public class AuthControllerImpl implements IAuthController {
     }
 
     @Override
+    public ResponseEntity<RegisterResponse> registerAdmin(@Valid @RequestBody RegisterRequest registerRequest) {
+        RegisterResponse response = registerService.registerAdmin(registerRequest);
+        return ResponseEntity.status(201).body(response);
+    }
+
+    @Override
     public ResponseEntity<AuthResponse> authenticate(@Valid @RequestBody AuthRequest authRequest) {
         AuthResponse response = authenticationService.login(authRequest);
         return ResponseEntity.ok(response);
     }
+
 }
