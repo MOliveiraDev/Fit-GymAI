422093b7fbdabb2a939556026f23a8297dd993ce
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-07-24 00:13:48 -0300

    Removed all

--- src/main/java/gemini/FitGymGpt/Exceptions/Gemini/GeminiApiException.java
+++
@@ -1,8 +0,0 @@
-package gemini.FitGymGpt.exceptions.gemini;
-
-public class GeminiApiException extends RuntimeException{
-
-    public GeminiApiException(String message) {
-        super(message);
-    }
-}
