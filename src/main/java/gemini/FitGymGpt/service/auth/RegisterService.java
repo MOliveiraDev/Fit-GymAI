58ecf277757ed71b3705cc0fb89bddefac6af25e
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-08-31 00:46:48 -0300

    implements registerValidationsList for register method

--- src/main/java/gemini/FitGymGpt/service/auth/RegisterService.java
+++ src/main/java/gemini/FitGymGpt/service/auth/RegisterService.java
@@ -4,10 +4,13 @@ import gemini.FitGymGpt.database.repository.UserRepository;
 import gemini.FitGymGpt.dto.register.RegisterRequest;
 import gemini.FitGymGpt.dto.register.RegisterResponse;
 import gemini.FitGymGpt.exceptions.register.EmailActuallyExistsException;
+import gemini.FitGymGpt.strategy.IRegisterValidations;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,17 +18,12 @@ public class RegisterService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final List<IRegisterValidations> registerValidationsList;
 
     @Transactional
     public RegisterResponse register(RegisterRequest request) {
 
-        if (!request.getPassword().equals(request.getConfirmPassword())) {
-            throw new IllegalArgumentException("As senhas não coincidem");
-        }
-
-        if (userRepository.existsByEmail(request.getEmail())) {
-            throw new EmailActuallyExistsException("Email já cadastrado");
-        }
+        registerValidationsList.forEach(strategy -> strategy.registerResponseValidations(request));
 
         var user = userRepository.save(
                 gemini.FitGymGpt.database.model.User.builder()
