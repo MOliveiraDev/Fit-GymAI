7b04f5c917d7dfb5b8ff88215326f3c1c33f34e3
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-11-26 21:22:31 -0300

    refactor: clean up imports in LogOutControllerImpl

--- src/main/java/gemini/FitGymGpt/controller/auth/Impl/LogOutControllerImpl.java
+++ src/main/java/gemini/FitGymGpt/controller/auth/Impl/LogOutControllerImpl.java
@@ -5,13 +5,9 @@ import gemini.FitGymGpt.service.jwt.TokenBlacklistService;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
