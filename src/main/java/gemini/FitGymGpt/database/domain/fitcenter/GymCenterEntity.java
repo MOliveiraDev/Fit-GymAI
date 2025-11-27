844e3699e4f07e10ae6595c31447084afa346567
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-11-27 20:38:14 -0300

    Feat: created GymCenterEntity

---
+++ src/main/java/gemini/FitGymGpt/database/domain/fitcenter/GymCenterEntity.java
@@ -0,0 +1,70 @@
+package gemini.FitGymGpt.database.domain.fitcenter;
+
+import gemini.FitGymGpt.database.domain.user.UserEntity;
+import gemini.FitGymGpt.enums.GymStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "gym_centers_tb")
+public class GymCenterEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long gymCenterId;
+
+    @Column(nullable = false, name = "gym_name")
+    @NotBlank(message = "Nome da academia não pode ser vazio")
+    private String gymCenterName;
+
+    @Column(name = "gym_description")
+    private String gymDescription;
+
+    @Column(name = "gym_phone")
+    private String gymPhone;
+
+    @Column(name = "gym_email")
+    @Email(message = "Forneça um email corporativo")
+    private String gymEmail;
+
+    @Column(name = "CNPJ", nullable = false)
+    private String TaxId;
+
+    @Column(name = "gym_website")
+    private String gymWebsite;
+
+    @Column(name = "gym_image_url")
+    private String gymImageUrl;
+
+    @Column(name = "gym_address", nullable = false)
+    @NotBlank(message = "Endereço da academia não pode ser vazio")
+    private String gymCenterAddress;
+
+    @Column(name = "gym_open_time")
+    @NotNull(message = "Horário de abertura é obrigatório")
+    private LocalTime openingTime;
+
+    @Column(name = "gym_close_time")
+    @NotNull(message = "Horário de fechamento é obrigatório")
+    private LocalTime closingTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner", nullable = false)
+    private UserEntity owner;
+
+    @OneToMany(mappedBy = "gymCenterEntity")
+    private Set<UserEntity> members = new HashSet<>();
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gym_status")
+    private GymStatus gymStatus = GymStatus.CLOSED;
+}
+
