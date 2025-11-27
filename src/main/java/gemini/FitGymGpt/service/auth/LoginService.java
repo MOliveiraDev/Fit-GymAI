485b3846d18b5195acad2363636398a844095071
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-11-26 21:06:23 -0300

    refactor: rename AuthenticationService to LoginService and update related imports

--- src/main/java/gemini/FitGymGpt/service/auth/LoginService.java
+++ src/main/java/gemini/FitGymGpt/service/auth/LoginService.java
@@ -1,10 +1,15 @@
 package gemini.FitGymGpt.service.auth;
 
-import gemini.FitGymGpt.dto.auth.AuthRequest;
-import gemini.FitGymGpt.dto.auth.AuthResponse;
-import gemini.FitGymGpt.exceptions.auth.EmailNotFoundException;
-import gemini.FitGymGpt.exceptions.auth.PasswordIsIncorrectException;
+
+import gemini.FitGymGpt.database.domain.user.UserEntity;
+import gemini.FitGymGpt.database.domain.user.UserStatus;
+import gemini.FitGymGpt.database.repository.user.UserRepository;
+import gemini.FitGymGpt.dto.login.AuthRequest;
+import gemini.FitGymGpt.dto.login.AuthResponse;
+import gemini.FitGymGpt.exception.login.EmailNotFoundException;
+import gemini.FitGymGpt.exception.login.PasswordIsIncorrectException;
 import gemini.FitGymGpt.service.jwt.JwtService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -15,28 +20,37 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class AuthenticationService{
+public class LoginService {
 
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserRepository userRepository;
 
+    @Transactional
     public AuthResponse login(AuthRequest request) {
-        try {
+
+        try{
             Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+                    new UsernamePasswordAuthenticationToken(
+                            request.email(),
+                            request.password()
+                    )
             );
+
+
+            UserEntity user = (UserEntity) authentication.getPrincipal();
+            user.setUserStatus(UserStatus.ONLINE);
+            userRepository.save(user);
 
             UserDetails userDetails = (UserDetails) authentication.getPrincipal();
             String jwtToken = jwtService.generateToken(userDetails);
 
-            return AuthResponse.builder()
-                    .email(userDetails.getUsername())
-                    .token(jwtToken)
-                    .build();
+            return new AuthResponse(jwtToken);
         } catch (BadCredentialsException e) {
             throw new PasswordIsIncorrectException("Senha incorreta");
-    }   catch (Exception e ) {
+        }   catch (Exception e ) {
             throw new EmailNotFoundException("Email não encontrado");
         }
     }
+
 }
