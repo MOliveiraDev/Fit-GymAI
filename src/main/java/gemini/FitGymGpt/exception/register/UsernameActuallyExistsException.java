88dc37b6f75a0cbba6efd3cdf89e1a790a3ae48d
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-11-26 21:21:54 -0300

    refactor: remove overloaded constructor from UsernameActuallyExistsException

--- src/main/java/gemini/FitGymGpt/exception/register/UsernameActuallyExistsException.java
+++ src/main/java/gemini/FitGymGpt/exception/register/UsernameActuallyExistsException.java
@@ -5,8 +5,4 @@ public class UsernameActuallyExistsException extends RuntimeException {
     public UsernameActuallyExistsException(String username) {
         super("O nome de usuário " + username + " já está cadastrado.");
     }
-
-    public UsernameActuallyExistsException(String username, Throwable cause) {
-        super("O nome de usuário " + username + " já está cadastrado.", cause);
-    }
 }
