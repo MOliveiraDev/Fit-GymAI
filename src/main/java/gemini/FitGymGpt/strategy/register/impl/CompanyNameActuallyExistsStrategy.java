c3691004b9ca4f5ed1a7698a1b402e75cff3984c
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-11-28 20:31:46 -0300

    refactor: add validation in parameters and convert company name to string

--- src/main/java/gemini/FitGymGpt/strategy/register/impl/CompanyNameActuallyExistsStrategy.java
+++ src/main/java/gemini/FitGymGpt/strategy/register/impl/CompanyNameActuallyExistsStrategy.java
@@ -1,7 +1,9 @@
 package gemini.FitGymGpt.strategy.register.impl;
 
 import gemini.FitGymGpt.database.repository.fitcenter.GymCenterEntityRepository;
+import gemini.FitGymGpt.dto.register.ceotrainer.CeoTrainerRegisterRequest;
 import gemini.FitGymGpt.strategy.register.ICeoRegisterValidations;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
@@ -14,8 +16,8 @@ public class CompanyNameActuallyExistsStrategy implements ICeoRegisterValidation
 
     @Override
     @SneakyThrows
-    public void registerCeoResponseValidations(gemini.FitGymGpt.dto.register.ceotrainer.CeoTrainerRegisterRequest request) {
-        if (gymCenterEntityRepository.findByGymCenterName(request.companyName())) {
+    public void registerCeoResponseValidations(@Valid CeoTrainerRegisterRequest request) {
+        if (gymCenterEntityRepository.findByGymCenterName(request.companyName().toString())) {
             throw new IllegalArgumentException("Essa empresa já está cadastrada");
         }
 
