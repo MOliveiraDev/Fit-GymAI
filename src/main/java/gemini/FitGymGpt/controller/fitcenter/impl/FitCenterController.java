adaa8cf5ed4dc5c362aadef86e10acfad9eabead
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-12-02 16:06:41 -0300

    feat: add controller methods create fit and update fit

--- src/main/java/gemini/FitGymGpt/controller/fitcenter/impl/FitCenterController.java
+++ src/main/java/gemini/FitGymGpt/controller/fitcenter/impl/FitCenterController.java
@@ -1,12 +1,30 @@
 package gemini.FitGymGpt.controller.fitcenter.impl;
 
 import gemini.FitGymGpt.controller.fitcenter.IFitCenterController;
+import gemini.FitGymGpt.dto.fitcenter.GymCenterRequest;
+import gemini.FitGymGpt.dto.fitcenter.GymCenterResponse;
+import gemini.FitGymGpt.service.fitcenter.GymCenterService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/fitcenter")
 public class FitCenterController implements IFitCenterController {
+
+    private final GymCenterService gymCenterService;
+
+    @Override
+    public ResponseEntity<GymCenterResponse> createFitCenter(@Valid @RequestBody GymCenterRequest request) {
+        GymCenterResponse response = gymCenterService.createGymCenter(request);
+        return ResponseEntity.status(201).body(response);
+    }
+
+    @Override
+    public ResponseEntity<GymCenterResponse> updateFitCenter(@PathVariable Long gymCenterId, @RequestBody GymCenterRequest request) {
+        GymCenterResponse response = gymCenterService.updateGymCenter(gymCenterId, request);
+        return ResponseEntity.status(201).body(response);
+    }
 }
