7e2612fb10c950c11a5e931d83f3b9f8faa00a80
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-12-03 23:10:00 -0300

    refactor: removed Set GymCenterEntity for yearsExperience Integer

--- src/main/java/gemini/FitGymGpt/dto/register/ceotrainer/CeoTrainerRegisterRequest.java
+++ src/main/java/gemini/FitGymGpt/dto/register/ceotrainer/CeoTrainerRegisterRequest.java
@@ -1,7 +1,6 @@
 package gemini.FitGymGpt.dto.register.ceotrainer;
 
-import gemini.FitGymGpt.database.domain.fitcenter.GymCenterEntity;
-import jakarta.validation.constraints.NotBlank;
+
 
 import java.util.Date;
 import java.util.Set;
@@ -14,7 +13,6 @@ public record CeoTrainerRegisterRequest(
              Date birthDate,
              String gender,
 
-            @NotBlank(message = "Nome da empresa é obrigatório")
-             Set<GymCenterEntity> companyName
+             Integer yearsExperience
     )  {}
 
