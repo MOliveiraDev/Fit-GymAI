a4876e983176ea5e377cbd07d6bb2508a19a2fbf
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-12-03 23:12:38 -0300

    refactor: adjusting swagger api version

--- src/main/java/gemini/FitGymGpt/config/swagger/OpenAPIConfig.java
+++ src/main/java/gemini/FitGymGpt/config/swagger/OpenAPIConfig.java
@@ -17,7 +17,7 @@ public class OpenAPIConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("FitGymAI API")
-                        .version("1.1")
+                        .version("1.5.0")
                         .description("API para gerenciamento de usuários, planos de treino, com autenticação JWT e integração com a IA Gemini Flash 2.0."))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new io.swagger.v3.oas.models.Components()
