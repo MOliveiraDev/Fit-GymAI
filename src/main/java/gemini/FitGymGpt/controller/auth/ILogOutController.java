f64be7eddd53f37c71b377a3c5726983fd35d9a1
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-12-03 23:11:07 -0300

    feat: add tag name

--- src/main/java/gemini/FitGymGpt/controller/auth/ILogOutController.java
+++ src/main/java/gemini/FitGymGpt/controller/auth/ILogOutController.java
@@ -3,10 +3,12 @@ package gemini.FitGymGpt.controller.auth;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
+@Tag(name = "Logout")
 public interface ILogOutController {
 
     @Operation(summary = "Log out userEntity", description = "Logs out the userEntity by invalidating the JWT token.")
