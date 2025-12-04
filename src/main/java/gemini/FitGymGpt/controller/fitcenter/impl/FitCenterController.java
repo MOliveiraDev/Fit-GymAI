41829a62e7f5dc633b9ea686c4fe929aa21af465
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-12-03 21:13:18 -0300

    feat: implements methods for list Gym Centers with Name, Open, Expired and Canceled

--- src/main/java/gemini/FitGymGpt/controller/fitcenter/impl/FitCenterController.java
+++ src/main/java/gemini/FitGymGpt/controller/fitcenter/impl/FitCenterController.java
@@ -6,12 +6,13 @@ import gemini.FitGymGpt.dto.fitcenter.GymCenterResponse;
 import gemini.FitGymGpt.service.fitcenter.GymCenterService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/fitcenter")
+@RequestMapping("/api/fitCenter")
 public class FitCenterController implements IFitCenterController {
 
     private final GymCenterService gymCenterService;
@@ -23,8 +24,29 @@ public class FitCenterController implements IFitCenterController {
     }
 
     @Override
-    public ResponseEntity<GymCenterResponse> updateFitCenter(@PathVariable Long gymCenterId, @RequestBody GymCenterRequest request) {
+    public ResponseEntity<GymCenterResponse> updateGymCenter(Long gymCenterId, @Valid @RequestBody GymCenterRequest request) {
         GymCenterResponse response = gymCenterService.updateGymCenter(gymCenterId, request);
         return ResponseEntity.status(201).body(response);
+    }
+
+    @Override
+    public ResponseEntity<?> listGymCentersWithName(String name, int page, int size) {
+        return ResponseEntity.status(200).body(gymCenterService.findGymCenterByName(name, PageRequest.of(page, size)));
+    }
+
+    @Override
+    public ResponseEntity<?> listOpenGymCenters(int page, int size) {
+        return ResponseEntity.status(200).body(gymCenterService.findByGymCenterOpened(PageRequest.of(page, size)));
+    }
+
+    @Override
+    public ResponseEntity<?> listExpiredGymCenters(int page, int size) {
+        return ResponseEntity.status(200).body(gymCenterService.findByGymCenterExpired(PageRequest.of(page, size)));
+    }
+
+    @Override
+    public ResponseEntity<?> listCanceledGymCenters(int page, int size) {
+        return ResponseEntity.status(200).body(gymCenterService.findByGymCenterCanceled(PageRequest.of(page, size)));
+
     }
 }
