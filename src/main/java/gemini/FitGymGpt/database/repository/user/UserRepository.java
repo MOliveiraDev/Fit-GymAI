c0ac0de6e9aaee4936f2161a8cf45814e787bcdf
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-12-03 23:08:51 -0300

    feat: add new methods to UserRepository for email existence check and user retrieval by UUID

--- src/main/java/gemini/FitGymGpt/database/repository/user/UserRepository.java
+++ src/main/java/gemini/FitGymGpt/database/repository/user/UserRepository.java
@@ -2,12 +2,18 @@ package gemini.FitGymGpt.database.repository.user;
 
 import gemini.FitGymGpt.database.domain.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface UserRepository extends JpaRepository <UserEntity, Long> {
 
     Optional<UserEntity> findByEmail(String email);
 
     boolean findByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    Optional<Object> findByUserId(UUID userId);
 }
